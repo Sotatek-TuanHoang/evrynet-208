b9807a9581fd82107f40054f683dcf178f99fb68
Your Name <tuan.hoang@sotatek.com>
2021-06-15 09:29:56 +0700

    update

--- start_test_nodes.sh
+++ start_test_nodes.sh
@@ -10,8 +10,8 @@ do
   gev --datadir ./tests/test_nodes/node"$i"/data init ./tests/test_nodes/genesis.json
 
   echo "--- Start test node $i ..."
-  gev --datadir ./tests/test_nodes/node"$i"/data --rpccorsdomain "http://localhost:8080" --nodiscover --tendermint.blockperiod 1 --syncmode full --networkid 15 --mine \
+  gev --datadir ./tests/test_nodes/node"$i"/data --rpccorsdomain "*" --targetgaslimit '900000000000000' --nodiscover --tendermint.blockperiod 1 --syncmode full --networkid 15 --mine \
     --rpc --rpcaddr 0.0.0.0 --rpcport 2200"$i" --port 3030"$i" \
     --pprof --pprofport 606"$i" \
-    --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,tendermint --allow-insecure-unlock 2>>node"$i".log &
+    --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,tendermint --allow-insecure-unlock 2>>node"$i".log & 
 done
